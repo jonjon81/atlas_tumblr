a70dd551e63e3d30d38aa75d8f7c2c834d5aa122
Jon Atlas <jjjatlas@hotmail.com>
2016-08-03 17:03:53 -0400

    Add comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,19 +22,24 @@ class PostsController < ApplicationController
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
 
-  end
-
-  def update
-
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
 
+    redirect_to root_path
   end
 
   private
